9d2f71137c083a3802a02d0d359fe9e24e30a67d
Tiran Meltser <tiran.meltser@efrat.com>
2020-02-25 20:25:34 +0200

    update

---
+++ vars/failReplayedBuild.groovy
@@ -0,0 +1,14 @@
+//
+// Custom step:	prevents from replayed builds to exexute,
+// 					as otherwise such builds will generate artifacts (like docker images or helm charts)
+//						with old/obsolete/stale code 
+//
+def failReplayedBuild(String buildID) {
+	// Check if this is a replay of an old build and if so, stop the build
+	if (currentBuild.nextBuild == null) {
+		error "This is a replay of build [${buildID}] which is not allowed - build is stopped!"
+	}
+	else {
+		echo "Build [${buildID}] is a new build - build is allowed to continue."
+	}
+}
