8da2990f1833fe802a15b041351345ba3434de9e
Tiran Meltser <tiran.meltser@efrat.com>
2020-02-25 21:50:53 +0200

    update

--- vars/failReplayedBuild.groovy
+++ vars/failReplayedBuild.groovy
@@ -5,7 +5,7 @@
 //
 def call(String buildID) {
 	// Check if this is a replay of an old build and if so, stop the build
-	if (currentBuild.nextBuild != null) {
+	if (currentBuild.displayName != null || currentBuild.description != null) {
 		error "This is a replay of build [${buildID}] which is not allowed - build is stopped!"
 	}
 	else {
