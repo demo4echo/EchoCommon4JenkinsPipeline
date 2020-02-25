df22d3c882c2c9ab629d3a7eee695d942d5e536e
Tiran Meltser <tiran.meltser@efrat.com>
2020-02-25 22:20:06 +0200

    update

--- vars/failReplayedBuild.groovy
+++ vars/failReplayedBuild.groovy
@@ -4,15 +4,14 @@
 //						with old/obsolete/stale code 
 //
 def call(String buildID) {
-	def buildDisplayName = currentBuild.displayName
-	def buildDecription = currentBuild.description
-
-	echo "Current Display Name is: [${buildDisplayName}]"
-	echo "Current Decription is: [${buildDecription}]"
+	// https://stackoverflow.com/questions/51555910/how-to-know-inside-jenkinsfile-script-that-current-build-is-an-replay/52302879#52302879
+	def isBuildReplay = currentBuild.rawBuild.getCauses().any {
+		cause -> cause instanceof org.jenkinsci.plugins.workflow.cps.replay.ReplayCause
+	}
 
 	// Check if this is a replay of an old build and if so, stop the build
-	if (buildDecription != null) {
-		error "Build [${buildID}] is a replay of build [${buildDisplayName}] which is not allowed - build is stopped!"
+	if (isBuildReplay == true) {
+		error "Build [${buildID}] is a replay of an old build, which is not allowed - build is stopped!"
 	}
 	else {
 		echo "Build [${buildID}] is a new build - build is allowed to continue."
