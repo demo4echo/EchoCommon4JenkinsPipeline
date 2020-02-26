3a4e5a2f68353df9708d4416e38f372e458b1e17
Tiran Meltser <tiran.meltser@efrat.com>
2020-02-26 16:18:07 +0200

    update

--- vars/updateBuildInformation.groovy
+++ vars/updateBuildInformation.groovy
@@ -5,6 +5,6 @@ def call() {
 	def primeVersion = sh(script: "./gradlew -Preckon.scope=${env.JENKINS_SLAVE_K8S_RECKON_SCOPE} -Preckon.stage=${env.JENKINS_SLAVE_K8S_RECKON_STAGE} -Pdemo4echo.designatedTagName=${params.DESIGNATED_VERSION} printApplicableVersion | grep Prime | awk '{print \$3}'", returnStdout: true)
 	def k8sJenkinsSlaveNodeName = sh(script: 'echo $NODE_HOST_NAME_ENV_VAR', returnStdout: true)
 				
-	buildName "#${env.BUILD_NUMBER}" + primeVersion.? : "|v${primeVersion}"
+	buildName "#${env.BUILD_NUMBER}" + primeVersion ?: "|v${primeVersion}"
 	buildDescription "@${k8sJenkinsSlaveNodeName}"
 }
