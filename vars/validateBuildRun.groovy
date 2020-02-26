9b6132c45901a18f93cfa9ad2fc0a309b664a03e
Tiran Meltser <tiran.meltser@efrat.com>
2020-02-26 13:56:33 +0200

    update

--- vars/validateBuildRun.groovy
+++ vars/validateBuildRun.groovy
@@ -8,13 +8,14 @@
 //
 def call(String buildID = env.BUILD_ID,boolean allowReplayedBuild = false) {
 	// https://stackoverflow.com/questions/51555910/how-to-know-inside-jenkinsfile-script-that-current-build-is-an-replay/52302879#52302879
-	def isBuildReplay = currentBuild.rawBuild.getCauses().any {
+	// https://javadoc.jenkins.io/plugin/workflow-cps/org/jenkinsci/plugins/workflow/cps/replay/ReplayCause.html
+	def replayCause = currentBuild.rawBuild.getCauses().any {
 		cause -> cause instanceof org.jenkinsci.plugins.workflow.cps.replay.ReplayCause
 	}
 
 	// Check applicable conditions on build run
-	if (allowReplayedBuild == false && isBuildReplay == true) {
-		error "Build [${buildID}] is a replay of an old build, which is not allowed - build is stopped!"
+	if (allowReplayedBuild == false && replayCause != null) {
+		error "Build [${buildID}] is a replay of build [#${replayCause.getOriginalNumber()}], which is not allowed - build is stopped!"
 	}
 	else {
 		echo "Build [${buildID}] is a new build - build is allowed to continue."
