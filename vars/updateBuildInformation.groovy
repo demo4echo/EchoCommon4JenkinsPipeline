99c38609943dfea5eb29c3fdea21522b28973aea
Tiran Meltser <tiran.meltser@efrat.com>
2020-03-13 23:52:22 +0200

    update

--- vars/updateBuildInformation.groovy
+++ vars/updateBuildInformation.groovy
@@ -1,9 +1,9 @@
 //
 // Custom step: Update build name and build description with relevant build data
 //
-def call() {
+def call(boolean assimilateVersionIntoBuildName = true) {
 	def primeVersion = sh(script: "./gradlew -Preckon.scope=${env.JENKINS_SLAVE_K8S_RECKON_SCOPE} -Preckon.stage=${env.JENKINS_SLAVE_K8S_RECKON_STAGE} -Pdemo4echo.designatedTagName=${params.DESIGNATED_VERSION} -Pdemo4echo.designatedTagMessage='${params.DESIGNATED_VERSION_MESSAGE}' printApplicableVersion | grep Prime | awk '{print \$3}'", returnStdout: true)
-	def adjustedBuildVersion = primeVersion ? "|v${primeVersion}" : ""
+	def adjustedBuildVersion = primeVersion && assimilateVersionIntoBuildName == true ? "|v${primeVersion}" : ""
 	def k8sJenkinsSlaveNodeName = sh(script: 'echo $NODE_HOST_NAME_ENV_VAR', returnStdout: true)
 
 	buildName "#${env.BUILD_NUMBER}${adjustedBuildVersion}"
