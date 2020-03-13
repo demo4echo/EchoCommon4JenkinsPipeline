aa58c46a1eb1fa847f3d8429d4baf6c20d2eb706
Tiran Meltser <tiran.meltser@efrat.com>
2020-03-13 18:33:59 +0200

    update

--- vars/updateBuildInformation.groovy
+++ vars/updateBuildInformation.groovy
@@ -2,7 +2,7 @@
 // Custom step: Update build name and build description with relevant build data
 //
 def call() {
-	def primeVersion = sh(script: "./gradlew -Preckon.scope=${env.JENKINS_SLAVE_K8S_RECKON_SCOPE} -Preckon.stage=${env.JENKINS_SLAVE_K8S_RECKON_STAGE} -Pdemo4echo.designatedTagName=${params.DESIGNATED_VERSION} printApplicableVersion | grep Prime | awk '{print \$3}'", returnStdout: true)
+	def primeVersion = sh(script: "./gradlew -Preckon.scope=${env.JENKINS_SLAVE_K8S_RECKON_SCOPE} -Preckon.stage=${env.JENKINS_SLAVE_K8S_RECKON_STAGE} -Pdemo4echo.designatedTagName=${params.DESIGNATED_VERSION} -Pdemo4echo.designatedTagMessage='${params.DESIGNATED_VERSION_MESSAGE}' printApplicableVersion | grep Prime | awk '{print \$3}'", returnStdout: true)
 	def adjustedBuildVersion = primeVersion ? "|v${primeVersion}" : ""
 	def k8sJenkinsSlaveNodeName = sh(script: 'echo $NODE_HOST_NAME_ENV_VAR', returnStdout: true)
 
