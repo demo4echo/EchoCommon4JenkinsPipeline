76686be2092c9c96a449fdda9c5b67b53ac8a2b3
Tiran Meltser <tiran.meltser@efrat.com>
2020-03-14 16:01:13 +0200

    update

--- vars/persistReleaseVersions.groovy
+++ vars/persistReleaseVersions.groovy
@@ -4,7 +4,12 @@
 //		releaseVersionsDataAsYamlStr:	The data (as a yaml string) to be saved in the designated file
 //			default:							N/A
 //
+// Note: This step is meant to be used by Jenkinsfile4Release which means no Gradle support can be used!
+//
 def call(String releaseVersionsDataAsYamlStr) {
 	// Persist (and update) the yaml file into the root of the repository
-	writeYaml file: "/root/.helm/${pipelineCommon.CONST_RELEASE_VERSIONS_FILE_NAME}", data: releaseVersionsDataAsYamlStr, overwrite: true
+	writeYaml file: "${pipelineCommon.CONST_RELEASE_VERSIONS_FILE_NAME}", data: releaseVersionsDataAsYamlStr, overwrite: true
+
+	echo 'Sleeping for 5 minutes'
+	sleep 300
 }
